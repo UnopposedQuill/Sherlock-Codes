3d2f706fc61b1e567309e0d8acb224ce7e8c5223
UnopposedQuill <UnopposedQuill@github.com>
2018-05-27 16:13:15 -0600

    Agregados los archivos iniciales para el proyecto, sin embargo, la versión de C utilizada originalmente no soporta el uso de Regex (expresiones regulares), así que se intentará de nuevo pero con las librerías nativas de linux

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
+#include <stdio.h>//librería de entrada/salida
+#include <stdlib.h>//librería de estándar de C
+#include <string.h>//necesito poder manejar strings arbitrariamente
+#include <regex.h>
 
 int main(int argc, char *argv[])
 {
-    printf("Hello World!\n");
+    //printf("Hello World!\n");
+    //primero debo cargar todos los archivos especificados por lo ingresado en la línea de comandos
+    for(int documento = 1;i < argc;i++){//debo empezar en 1 para saltarme la información de en dónde se está ejecutando esto
+
+    }
+
     return 0;
 }
